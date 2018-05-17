211805b7df997ac2c53f97ae3e54fad840c1b57e
Kamil Karczewski <serwisyrejestracje@wp.pl>
2018-05-17 17:06:07 +0200

    autoinstall
    - nerdtree
    - vundle
    - fugitive
    - jedi-vim - autocomple for python
    - vim clickable

---
+++ autoinstall.sh
@@ -0,0 +1,40 @@
+python3.6 -m pip install jedi
+python3.6 -m pip install flake8
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd .vim/bundle/ && git clone https://github.com/nvie/vim-flake8
+echo 'set nocompatible              " be iMproved, required' >> ~/.vimrc
+echo 'filetype off                  " required' >> ~/.vimrc
+echo '' >> ~/.vimrc
+echo '" set the runtime path to include Vundle and initialize' >> ~/.vimrc
+echo 'set rtp+=~/.vim/bundle/Vundle.vim' >> ~/.vimrc
+echo 'call vundle#begin()' >> ~/.vimrc
+echo "Plugin 'VundleVim/Vundle.vim'" >> ~/.vimrc
+echo "Plugin 'tpope/vim-fugitive'" >> ~/.vimrc
+echo "Plugin 'davidhalter/jedi-vim'" >> ~/.vimrc
+echo "Plugin 'scrooloose/nerdtree'" >> ~/.vimrc
+echo "Plugin 'Xuyuanp/nerdtree-git-plugin'" >> ~/.vimrc
+echo "Plugin 'Rykka/clickable.vim'" >> ~/.vimrc
+echo "Plugin 'Rykka/clickable-things'" >> ~/.vimrc
+echo '' >> ~/.vimrc
+echo 'call vundle#end()            " required' >> ~/.vimrc
+echo '" To ignore plugin indent changes, instead use:' >> ~/.vimrc
+echo '" filetype plugin on' >> ~/.vimrc
+echo '" :PluginList       - lists configured plugins' >> ~/.vimrc
+echo '" :PluginInstall    - installs plugins; append `!` to update or just' >> ~/.vimrc
+echo '" :PluginUpdate' >> ~/.vimrc
+echo '" :PluginSearch foo - searches for foo; append `!` to refresh local cache' >> ~/.vimrc
+echo '" :PluginClean      - confirms removal of unused plugins; append `!` to auto-approve removal' >> ~/.vimrc
+echo '"' >> ~/.vimrc
+echo 'execute pathogen#infect()' >> ~/.vimrc
+echo 'syntax on' >> ~/.vimrc
+echo 'filetype plugin indent on    " required' >> ~/.vimrc
+echo 'autocmd vimenter * NERDTree' >> ~/.vimrc
+echo 'autocmd StdinReadPre * let s:std_in=1' >> ~/.vimrc
+echo 'autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif' >> ~/.vimrc
+echo 'autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists("s:std_in") | exe "NERDTree" argv()[0] | wincmd p | ene | endif' >> ~/.vimrc
+echo 'set mouse=a' >> ~/.vimrc
+echo 'let g:NERDTreeMouseMode=3' >> ~/.vimrc
+echo 'autocmd Filetype python setlocal ts=3 sts=3 sw=3' >> ~/.vimrc
+echo 'set list' >> ~/.vimrc
+echo 'set listchars=tab:\ \ ,trail:.,extends:>,precedes:<' >> ~/.vimrc
