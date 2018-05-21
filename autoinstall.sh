4bb1a187f839cd76f88c277392af40b3be0259b0
Kamil Karczewski <serwisyrejestracje@wp.pl>
2018-05-21 10:43:31 +0200

    Update autoinstall.sh
    - added some plugins

--- autoinstall.sh
+++ autoinstall.sh
@@ -16,6 +16,16 @@ echo "Plugin 'scrooloose/nerdtree'" >> ~/.vimrc
 echo "Plugin 'Xuyuanp/nerdtree-git-plugin'" >> ~/.vimrc
 echo "Plugin 'Rykka/clickable.vim'" >> ~/.vimrc
 echo "Plugin 'Rykka/clickable-things'" >> ~/.vimrc
+echo "Plugin 'ervandew/supertab'" >> ~/.vimrc
+echo "Plugin 'raimondi/delimitmate'" >> ~/.vimrc
+echo "Plugin 'mattn/emmet-vim'" >> ~/.vimrc
+echo "Plugin 'ntpeters/vim-better-whitespace'" >> ~/.vimrc
+echo "Plugin 'junegunn/vim-easy-align'" >> ~/.vimrc
+echo "Plugin 'yggdroot/indentline'" >> ~/.vimrc
+echo "Plugin 'DrawIt'" >> ~/.vimrc
+echo "\"Plugin 'tpope/vim-sensible'" >> ~/.vimrc
+echo "\"Plugin 'elxr/vim-json'" >> ~/.vimrc
+echo "\"Plugin 'roxma/vim-paste-easy'" >> ~/.vimrc
 echo '' >> ~/.vimrc
 echo 'call vundle#end()            " required' >> ~/.vimrc
 echo '" To ignore plugin indent changes, instead use:' >> ~/.vimrc
@@ -26,6 +36,8 @@ echo '" :PluginUpdate' >> ~/.vimrc
 echo '" :PluginSearch foo - searches for foo; append `!` to refresh local cache' >> ~/.vimrc
 echo '" :PluginClean      - confirms removal of unused plugins; append `!` to auto-approve removal' >> ~/.vimrc
 echo '"' >> ~/.vimrc
+echo '" see :h vundle for more details or wiki for FAQ' >> ~/.vimrc
+echo '" Put your non-Plugin stuff after this line' >> ~/.vimrc
 echo 'execute pathogen#infect()' >> ~/.vimrc
 echo 'syntax on' >> ~/.vimrc
 echo 'filetype plugin indent on    " required' >> ~/.vimrc
@@ -33,8 +45,18 @@ echo 'autocmd vimenter * NERDTree' >> ~/.vimrc
 echo 'autocmd StdinReadPre * let s:std_in=1' >> ~/.vimrc
 echo 'autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif' >> ~/.vimrc
 echo 'autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists("s:std_in") | exe "NERDTree" argv()[0] | wincmd p | ene | endif' >> ~/.vimrc
+echo 'autocmd Filetype python setlocal ts=3 sts=3 sw=3' >> ~/.vimrc
+echo '' >> ~/.vimrc
+echo 'set list' >> ~/.vimrc
+echo 'set listchars=tab:\ \ ,trail:.,extends:>,procedes:<' >> ~/.vimrc
+echo 'set list lcs=tab:\|\ ' >> ~/.vimrc
 echo 'set mouse=a' >> ~/.vimrc
 echo 'let g:NERDTreeMouseMode=3' >> ~/.vimrc
-echo 'autocmd Filetype python setlocal ts=3 sts=3 sw=3' >> ~/.vimrc
-echo 'set list' >> ~/.vimrc
-echo 'set listchars=tab:\ \ ,trail:.,extends:>,precedes:<' >> ~/.vimrc
+echo 'let g:NERDTreeMinimalUI=1' >> ~/.vimrc
+echo 'let g:NERDTreeDirArrow=1' >> ~/.vimrc
+echo 'let g:indentLine_enabled=3' >> ~/.vimrc
+echo '"let g:indentLine_char=\'c\'' >> ~/.vimrc
+echo 'let g:indentLine_color_term=239' >> ~/.vimrc
+echo 'let g:indentLine_concealcursor=\'inc\'' >> ~/.vimrc
+echo 'let g:indentLine_conceallevel=1' >> ~/.vimrc
+
